da3aeabcb2d84cd6768d6062267026927a9ff322
[dyad] <git@dyad.sh>
2025-08-07 16:30:33 +0700

    [dyad] Removing Dyad branding from footer - wrote 1 file(s)

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const AppFooter = () => {
   return (
     <div className="p-4 text-center mt-auto hide-on-print">
-      <a
-        href="https://www.dyad.sh/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-      >
-        Made with Dyad
-      </a>
       <p className="text-xs text-gray-500 dark:text-gray-400 mb-1">SMKN 9 Pontianak</p>
       <p className="text-xs text-gray-500 dark:text-gray-400">Copyright by Jony Rabuansyah</p>
     </div>
